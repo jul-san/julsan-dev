4e0a44300d9f6ffa17d8285144021cdf09f25b58
Julian Sanchez <julianwsan@gmail.com>
2024-05-09 12:01:00 -0400

    trying to understand how pages and layouts work

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,13 @@
 import Image from "next/image";
-import Navbar from '../components/Navbar';
+import Navbar from '../app/components/Navbar';
 
 export default function Home() {
   return (
-    <>
-      <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-      width: '100%', // Ensure the container spans the full width
-    }}>
-      <div style={{
-        border: '1px solid black', // Add a border to create a box
-        padding: '10px', // Add some padding inside the box
-        width: '35%', // Set the width of the box
-        boxSizing: 'border-box' // Include padding and border in the width calculation
-      }}>
-              <Navbar backgroundColor="#20232a" textColor="#61dafb" />
-
+    <>   
         <p style={{ textAlign: 'left' }}>19 • senior @ florida state • jws22@fsu.edu</p>
         <p style={{ textAlign: 'left' }}>interests: systems software, intelligent machines, and human-computer interaction</p>
         <p style={{ textAlign: 'left' }}>other facts: raised in south florida, filipino-american, mediocre at chess, heat fan, loves to cook (and eat)</p>
         <p style={{ textAlign: 'left' }}>instagram • linkedin • spotify • x</p>
-      </div>
-    </div>
     </>
   );
 }
---
+++ app/resume/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function resume() {
+  return (
+    <h1>Julian
+    </h1>
+  )
+}
