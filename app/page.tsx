d6ef3530baee85935d9a694a3f8627c814782820
Julian Sanchez <julianwsan@gmail.com>
2024-05-09 16:42:18 -0400

    about "about"

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from '../app/components/Navbar';
 export default function Home() {
   return (
     <>   
-        <p style={{ textAlign: 'left' }}>19 • senior @ florida state • jws22@fsu.edu </p><br/>
+        <p style={{ textAlign: 'left' }}>about: 19, senior at florida state, comp sci major</p><br/>
         <p style={{ textAlign: 'left' }}>interests: systems software, intelligent machines, and human-computer interaction</p><br/>
         <p style={{ textAlign: 'left' }}>other facts: raised in south florida, filipino-american, mediocre at chess, heat fan, loves to cook (and eat)</p><br/>
         <p style={{ textAlign: 'left' }}>instagram • linkedin • spotify • x</p>
