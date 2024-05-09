edf157906b17c6ee97fb7d2f01359f45472e4be3
Julian Sanchez <julianwsan@gmail.com>
2024-05-09 12:53:06 -0400

    added experience

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-export default function resume() {
-  return (
-    <h1>Julian
-    </h1>
-  )
+export default function Resume() {
+  return (<>
+    <div> 
+      <p className='text-gray-500 text-sm'>summer research intern</p>
+      <p className='pb-2'>massachusetts institute of technology ↗</p>
+    </div>
+    <div> 
+      <p className='text-gray-500 text-sm'>data analytics intern</p>
+      <p className='pb-2'>devoe l. moore center ↗</p>
+    </div>
+    <div> 
+      <p className='text-gray-500 text-sm'>software engineer intern</p>
+      <p className='pb-2'>northrop grumman ↗</p>
+    </div>
+    <div> 
+      <p className='text-gray-500 text-sm'>internal vice president</p>
+      <p className='pb-2'> society of asian scientists and engineers ↗</p>
+    </div>
+    <div> 
+      <p className='text-gray-500 text-sm'>intructor</p>
+      <p className='pb-2'> code ninjas ↗</p>
+    </div>
+
+    </>)
 }
