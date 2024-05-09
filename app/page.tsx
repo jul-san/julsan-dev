57fd11d9b977cccbc34779e96989da7c8ddbbabb
Julian Sanchez <julianwsan@gmail.com>
2024-05-09 12:53:47 -0400

    added info

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Navbar from '../app/components/Navbar';
 export default function Home() {
   return (
     <>   
-        <p style={{ textAlign: 'left' }}>19 • senior @ florida state • jws22@fsu.edu</p>
-        <p style={{ textAlign: 'left' }}>interests: systems software, intelligent machines, and human-computer interaction</p>
-        <p style={{ textAlign: 'left' }}>other facts: raised in south florida, filipino-american, mediocre at chess, heat fan, loves to cook (and eat)</p>
+        <p style={{ textAlign: 'left' }}>19 • senior @ florida state • jws22@fsu.edu </p><br/>
+        <p style={{ textAlign: 'left' }}>interests: systems software, intelligent machines, and human-computer interaction</p><br/>
+        <p style={{ textAlign: 'left' }}>other facts: raised in south florida, filipino-american, mediocre at chess, heat fan, loves to cook (and eat)</p><br/>
         <p style={{ textAlign: 'left' }}>instagram • linkedin • spotify • x</p>
     </>
   );
