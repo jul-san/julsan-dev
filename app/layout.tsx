3800c6420bfbf03e7866eb25934961d9359a5651
Julian Sanchez <julianwsan@gmail.com>
2024-05-10 01:12:30 -0400

    put nav component in the right spot

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from '../app/components/Navbar';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,13 +15,13 @@ export default function RootLayout({children,}: Readonly<{ children: React.React
   return (
       <html lang="en">
         <body className={`flex justify-center items-center min-h-screen w-full ${inter.className}`}>
-          <div className="w-full max-w-lg mx-auto p-4 h-60">
-            <Navbar/>
-
+          <div className="w-full max-w-lg mx-auto p-4 h-60 -translate-y-40">
             <div className="text-left">
+            <Navbar/>
               {children}
             </div>
           </div>
+          <Script src="./node_modules/preline/dist/preline.js"></Script>
         </body>
       </html>
   );
