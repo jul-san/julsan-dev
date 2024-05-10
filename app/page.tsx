c8ffb9595a1bf54f4471972bb0877fc47bfdc4ba
Julian Sanchez <julianwsan@gmail.com>
2024-05-10 02:57:26 -0400

    changed links

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>   
-        <p className="text-left">about: 19, senior at florida state, comp sci major</p><br/>
+        <p className="text-left">about: 19, tallahassee, senior at florida state</p><br/>
         <p className="text-left">interests: systems software, intelligent machines, and human-computer interaction</p><br/>
         <p className="text-left">other facts: raised in south florida, filipino-american, mediocre at chess, heat fan, loves to cook (and eat)</p><br/>
         <p className="text-left">
-          <Link href="https://www.instagram.com/julianwsanchez/" className="underline">instagram</Link> •
-          <Link href="https://www.instagram.com/julianwsanchez/" className="underline">linkedin</Link> •
-          <Link href="https://www.instagram.com/julianwsanchez/" className="underline">spotify</Link> •
-          <Link href="https://www.instagram.com/julianwsanchez/" className="underline">x</Link> 
+          <Link href="https://www.instagram.com/julianwsanchez/" className="underline">instagram</Link>•
+          <Link href="https://www.linkedin.com/in/jul-san/" className="underline">linkedin</Link>•
+          <Link href="https://open.spotify.com/user/31nvmp25h2eqhcih2re43lwcsjee" className="underline">spotify</Link>•
+          <Link href="https://twitter.com/julianwsanchez" className="underline">x</Link> 
         </p> 
     </>
   );
