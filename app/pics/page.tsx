68c3b4d0437b84739b279da4c1b263319f5e14d1
Julian Sanchez <julianwsan@gmail.com>
2024-05-10 02:57:17 -0400

    added more images to the page

--- app/pics/page.tsx
+++ app/pics/page.tsx
@@ -3,16 +3,31 @@ import Image from 'next/image'
 
 export default function Pics() {
   return (<>
-      <p className="pb-2">me with some pretty cool people ;)</p>
+      <p className="pb-2">me with some pretty cool people</p>
       <Image src='/image/grid/vegas-tree.jpg' width={600} height={250} alt='vegas'/>
       <Image src='/image/grid/lana-eli.jpg' width={600} height={250} alt='vegas'/>
       <Image src='/image/grid/tallyween-cam-carissa-sami.jpg' width={600} height={250} alt='vegas'/>
       <Image src='/image/blog/serc/kyle-nina-i-serc.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/rachel.jpg' width={600} height={250} alt='vegas'/>
       <Image src='/image/grid/emi.jpg' width={600} height={250} alt='vegas'/>
-      <Image src='/image/grid/canyons.jpg' width={600} height={250} alt='vegas'/>
       <Image src='/image/grid/riceball-cat-lana-eva.jpg' width={600} height={250} alt='vegas'/>
       <Image src='/image/blog/atlanta/group-photobooth.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/nerds.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/uri-selfie.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/car-sami.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/matt-hotpot.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/fair-eva-lana-i.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/filipinos.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/faye.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/heart.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/ken-chess.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/dean-rhea.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/hmart-eva-selfie.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/kbbq-gainesville.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/eva-pie.jpg' width={600} height={250} alt='vegas'/>
+      <Image src='/image/grid/bonding.jpg' width={600} height={250} alt='vegas'/>
 
-      
+
+
     </>)
 }
