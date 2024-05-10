a41a610f5c05bd48af48647fcdd736fee43bf984
Julian Sanchez <julianwsan@gmail.com>
2024-05-10 02:57:08 -0400

    removed arrows

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,27 +4,27 @@ export default function Resume() {
   return (<>
     <div> 
       <p className='text-gray-500 text-sm'>machine learning research assistant</p>
-      <p className='pb-2'>famu-fsu college of engineering ↗</p>
+      <p className='pb-2'>famu-fsu college of engineering</p>
     </div>
     <div> 
       <p className='text-gray-500 text-sm'>summer research intern</p>
-      <p className='pb-2'>massachusetts institute of technology ↗</p>
+      <p className='pb-2'>massachusetts institute of technology</p>
     </div>
     <div> 
       <p className='text-gray-500 text-sm'>data analytics intern</p>
-      <p className='pb-2'>devoe l. moore center ↗</p>
+      <p className='pb-2'>devoe l. moore center</p>
     </div>
     <div> 
       <p className='text-gray-500 text-sm'>software engineer intern</p>
-      <p className='pb-2'>northrop grumman ↗</p>
+      <p className='pb-2'>northrop grumman</p>
     </div>
     <div> 
       <p className='text-gray-500 text-sm'>internal vice president</p>
-      <p className='pb-2'> society of asian scientists and engineers ↗</p>
+      <p className='pb-2'> society of asian scientists and engineers</p>
     </div>
     <div> 
-      <p className='text-gray-500 text-sm'>intructor</p>
-      <p className='pb-2'> code ninjas ↗</p>
+      <p className='text-gray-500 text-sm'>instructor</p>
+      <p className='pb-2'> code ninjas</p>
     </div>
 
     </>)
