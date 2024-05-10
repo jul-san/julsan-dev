3d90b20173676b9907925ef06e5c19bdb93fd77a
Julian Sanchez <julianwsan@gmail.com>
2024-05-10 01:12:13 -0400

    preline ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,11 +5,12 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    'node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {},
   },
   
-  plugins: [],
+  plugins: [require('preline/plugin'),],
 };
 export default config;
