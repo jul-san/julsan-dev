420fb83b6a642bee3b0c9908b04158d6642a0e31
Julian Sanchez <julianwsan@gmail.com>
2024-05-10 01:11:05 -0400

    removed underline

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,21 +2,15 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="bg-white-0 py-4">
+    <nav className="bg-white-0 py-2">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex">
-          <Link href="/" className="underline">
-            julian sanchez
-          </Link>&nbsp;&nbsp;
-          <Link href="/resume" className="underline">
-            resume
-          </Link>&nbsp;&nbsp;
-          <Link href="/blog" className="underline">
-            blog
-          </Link>&nbsp;&nbsp;
-          <Link href="/pics" className="underline">
-            pics
-          </Link>
+
+          <Link href="/" className="text-gray-500 hover:text-black">julian sanchez</Link>&nbsp;&nbsp;
+          <Link href="/resume" className="text-gray-500 hover:text-black">resume</Link>&nbsp;&nbsp;
+          <Link href="/blog" className="text-gray-500 hover:text-black">blog</Link>&nbsp;&nbsp;
+          <Link href="/pics" className="text-gray-500 hover:text-black">pics</Link>
+
         </div>
       </div>
     </nav>
