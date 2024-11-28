b8aed7a383604127d5a0917bfb7203828a6eca2a
Julian Sanchez <julianwsan@gmail.com>
2024-11-28 16:02:02 -0500

    💡 Simplified the Pics Page

--- app/pics/page.tsx
+++ app/pics/page.tsx
@@ -1,46 +1,55 @@
 import React from 'react'
 import Image from 'next/image'
 
+const IMAGE_PATHS = [
+  '/image/grid/boston-bench.jpg',
+  '/image/grid/lana-eli.jpg',
+  '/image/grid/last-night.jpg',
+  '/image/grid/hike.jpg',
+  '/image/grid/mit-id.jpg',
+  '/image/grid/best-chinese-res.jpg',
+  '/image/grid/tallyween-cam-carissa-sami.jpg',
+  '/image/grid/nyc.jpg',
+  '/image/grid/dragon-festival.jpg',
+  '/image/blog/serc/kyle-nina-i-serc.jpg',
+  '/image/grid/rock-climbing.jpg',
+  '/image/grid/rachel.jpg',
+  '/image/grid/emi.jpg',
+  '/image/grid/game-night.jpg',
+  '/image/grid/nyc-uri.jpg',
+  '/image/grid/riceball-cat-lana-eva.jpg',
+  '/image/grid/celtics.jpg',
+  '/image/blog/atlanta/group-photobooth.jpg',
+  '/image/grid/beach.jpg',
+  '/image/grid/nerds.jpg',
+  '/image/grid/uri-selfie.jpg',
+  '/image/grid/friendship-bbq.jpg',
+  '/image/grid/car-sami.jpg',
+  '/image/grid/matt-hotpot.jpg',
+  '/image/grid/fair-eva-lana-i.jpg',
+  '/image/grid/kimball-farms.jpg',
+  '/image/grid/filipinos.jpg',
+  '/image/grid/hunan.jpg',
+  '/image/grid/heart.jpg',
+  '/image/grid/ken-chess.jpg',
+  '/image/grid/hmart-eva-selfie.jpg',
+  '/image/grid/kbbq-gainesville.jpg',
+  '/image/grid/eva-pie.jpg'
+];
+
 export default function Pics() {
-  const imageStyle = { margin: '15px 0' }; // Adjust the value as needed
+  return (
+  <>
+    <p className="pb-2">me with some cool people</p>
+    
+    {IMAGE_PATHS.map((IMAGES, index) =>(
+      <Image key={index} 
+        style={{margin: '15px 0'}} 
+        src={IMAGES} 
+        width={600} 
+        height={250} 
+        alt=''/>)
+    )}
 
-  return (<>
-        <p className="pb-2">me with some cool people</p>
-        <Image style={imageStyle} src='/image/grid/lana-eli.jpg' width={600} height={250} alt='vegas'/>
-        <Image style={imageStyle} src='/image/grid/hike.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/mit-id.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/tallyween-cam-carissa-sami.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/nyc.jpg' width={600} height={250} alt='vegas'/>
-
-      <Image style={imageStyle} src='/image/grid/dragon-festival.jpg' width={600} height={250} alt='vegas'/>
-
-      <Image style={imageStyle} src='/image/blog/serc/kyle-nina-i-serc.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/rock-climbing.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/rachel.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/emi.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/nyc-uri.jpg' width={600} height={250} alt='vegas'/>
-
-      <Image style={imageStyle} src='/image/grid/riceball-cat-lana-eva.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/celtics.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/blog/atlanta/group-photobooth.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/beach.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/nerds.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/uri-selfie.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/friendship-bbq.jpg' width={600} height={250} alt='vegas'/>
-
-      <Image style={imageStyle} src='/image/grid/car-sami.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/matt-hotpot.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/fair-eva-lana-i.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/kimball-farms.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/filipinos.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/faye.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/hunan.jpg' width={600} height={250} alt='vegas'/>
-
-      <Image style={imageStyle} src='/image/grid/heart.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/ken-chess.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/dean-rhea.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/hmart-eva-selfie.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/kbbq-gainesville.jpg' width={600} height={250} alt='vegas'/>
-      <Image style={imageStyle} src='/image/grid/eva-pie.jpg' width={600} height={250} alt='vegas'/>
-    </>)
+  </>)
 }
