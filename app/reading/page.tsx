90ca7077d4016623ae08b59d4d5ede6be8a4dd2d
Julian Sanchez <julianwsan@gmail.com>
2024-12-08 00:45:57 -0500

    ❌ Removed extra gap

--- app/reading/page.tsx
+++ app/reading/page.tsx
@@ -9,19 +9,19 @@ export default function reading() {
       <p className='text-gray-500 text-sm'>aug 2024</p>
       <p className='pb-2'><i>dataclysm </i>| christian rudder</p>
 
-    </div><br/>
+    </div>
       <div> 
       <p className='text-gray-500 text-sm'>jul 2024</p>
       <p className='pb-2'><i>the enneagram </i>| helen palmer</p>
       <p className='pb-2'><i>gifts differing </i>| isabel briggs myers & peter b. myers</p>
 
-    </div><br/>
+    </div>
     <div> 
       <p className='text-gray-500 text-sm'>jun 2024</p>
       <p className='pb-2'><i>the body keeps the score </i>| bessel van del kolk</p>
       <p className='pb-2'><i>crying in h mart </i>| michelle zauner</p>
       <p className='pb-2'><i>minor feelings </i>| cathy park hong</p>
-    </div><br/>
+    </div>
     <div> 
       <p className='text-gray-500 text-sm'>may 2024</p>
       <p className='pb-2'><i>a thousand brains </i>| jeff hawkins</p>
@@ -29,21 +29,21 @@ export default function reading() {
       <p className='pb-2'><i>making a scene</i> | constance wu</p>
       <p className='pb-2'><i>in order to live </i>| yeonmi park</p>
       <p className='pb-2'><i>how to win friends & influence people </i>| dale carnegie</p>
-    </div><br/>
+    </div>
     <div> 
       <p className='text-gray-500 text-sm'>apr 2024</p>
       <p className='pb-2'><i>world travel: an irreverent guide </i>| anthony bourdain</p>
       <p className='pb-2'><i>personlity type </i>| lenore thomson</p>
-    </div><br/>
+    </div>
     <div> 
       <p className='text-gray-500 text-sm'>mar 2024</p>
       <p className='pb-2'><i>a gentlemen in moscow</i> | amor towles</p>
-    </div><br/>
+    </div>
     <div> 
       <p className='text-gray-500 text-sm'>feb 2024</p>
       <p className='pb-2'><i>everything i never told you </i>| celeste ng</p>
       <p className='pb-2'><i>we were dreamers </i>| simu liu</p>    
-      </div><br/>
+      </div>
     <div> 
       <p className='text-gray-500 text-sm'>jan 2024</p>
       <p className='pb-2'><i>how to american </i>| jimmy o. yang</p>
@@ -52,7 +52,7 @@ export default function reading() {
       <p className='pb-2'><i>the paper menagerie & other stories </i>| ken liu</p>    
 
 
-      </div><br/>    
+      </div>
       
     </>)
 }
