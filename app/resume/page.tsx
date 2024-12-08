06dd7a4364384f93cc0721ce3dea58f244508643
Julian Sanchez <julianwsan@gmail.com>
2024-12-08 00:43:47 -0500

    📝 Simplification

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,31 +1,24 @@
 import React from 'react'
+import Link from "next/link";
+import Job from '../components/Work/Job';
+
+const EXPERIENCE = [
+  {title: "full-stack engineering intern", company: "vitaltrack health"},
+  {title: "undergraduate research assistant", company: "florida state university – makex lab"},
+  {title: "summer research program intern", company: "massachusetts institute of technology – lincoln laboratory"},
+  {title: "data analytics intern", company: "devoe l. moore center"},
+  {title: "software engineering intern", company: "northrop grumman"},
+]
 
 export default function Resume() {
   return (<>
-    <div> 
-      <p className='text-gray-500 text-sm'>research assistant</p>
-      <p className='pb-2'>florida state university – makex laboratory</p>
-    </div>
-    <div> 
-      <p className='text-gray-500 text-sm'>summer research intern</p>
-      <p className='pb-2'>massachusetts institute of technology – lincoln laboratory</p>
-    </div>
-    <div> 
-      <p className='text-gray-500 text-sm'>data science intern</p>
-      <p className='pb-2'>devoe l. moore center</p>
-    </div>
-    <div> 
-      <p className='text-gray-500 text-sm'>software engineering intern</p>
-      <p className='pb-2'>northrop grumman</p>
-    </div>
-    <div> 
-      <p className='text-gray-500 text-sm'>internal vice president</p>
-      <p className='pb-2'> society of asian scientists and engineers</p>
-    </div>
-    <div> 
-      <p className='text-gray-500 text-sm'>instructor</p>
-      <p className='pb-2'> code ninjas</p>
-    </div>
 
-    </>)
+    {EXPERIENCE.map((item, index) => (
+      <Job key={index} 
+        title={item.title} 
+        company={item.company}/>
+      ))
+    }
+    
+  </>)
 }
