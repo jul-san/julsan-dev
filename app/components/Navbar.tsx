39273dbd2218d2e9335fe08dea9671ecc9bef36c
Julian Sanchez <julianwsan@gmail.com>
2024-12-08 00:44:37 -0500

    🔨 Changed navbar organization

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,14 +4,12 @@ const Navbar = () => {
   return (
     <nav className="bg-white-0 py-2">
       <div className="container mx-auto flex items-center justify-between">
+      <Link href="/" className="text-gray-500 hover:text-black">julian sanchez</Link>&nbsp;&nbsp;
         <div className="flex">
-
-          <Link href="/" className="text-gray-500 hover:text-black">julian sanchez</Link>&nbsp;&nbsp;
-          <Link href="/resume" className="text-gray-500 hover:text-black">resume</Link>&nbsp;&nbsp;
+          <Link href="/resume" className="text-gray-500 hover:text-black">work</Link>&nbsp;&nbsp;
           <Link href="/blog" className="text-gray-500 hover:text-black">blog</Link>&nbsp;&nbsp;
           <Link href="/reading" className="text-gray-500 hover:text-black">reading</Link>&nbsp;&nbsp;
           <Link href="/pics" className="text-gray-500 hover:text-black">pics</Link>&nbsp;&nbsp;
-
         </div>
       </div>
     </nav>
