6b1a23e54984d3d041bcf57faf2e02bdf294186d
Julian Sanchez <julianwsan@gmail.com>
2025-01-28 22:36:16 -0500

    styled navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,28 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="bg-white-0 py-2">
       <div className="container mx-auto flex items-center justify-between">
-      <Link href="/" className="text-gray-500 hover:text-black">julian sanchez</Link>&nbsp;&nbsp;
+        <Link href="/" className="font-dohyeon text-3xl">
+          Julian Sanchez
+        </Link>
+        &nbsp;&nbsp;
         <div className="flex">
-          <Link href="/resume" className="text-gray-500 hover:text-black">work</Link>&nbsp;&nbsp;
-          <Link href="/blog" className="text-gray-500 hover:text-black">blog</Link>&nbsp;&nbsp;
-          <Link href="/reading" className="text-gray-500 hover:text-black">reading</Link>&nbsp;&nbsp;
-          <Link href="/pics" className="text-gray-500 hover:text-black">pics</Link>&nbsp;&nbsp;
+          <Link
+            href="/blog"
+            className="font-dohyeon text-gray-500 hover:text-black"
+          >
+            Blog
+          </Link>
+          &nbsp;&nbsp;
+          <Link
+            href="/pics"
+            className="font-dohyeon text-gray-500 hover:text-black"
+          >
+            Pics
+          </Link>
+          &nbsp;&nbsp;
         </div>
       </div>
     </nav>
